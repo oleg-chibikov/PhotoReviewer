5aa237fa1a6e5d52e9c34901155cf4520c2b904a
Oleg Chibikov <ochibikov@platius.ru>
2016-05-27 16:45:46 +0300

    FullHeight button, arrange windows considering first opened window height and top

--- PhotoView.xaml.cs
+++ PhotoView.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Interop;
 using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
 using JetBrains.Annotations;
 using KeyEventArgs = System.Windows.Input.KeyEventArgs;
 
@@ -29,15 +30,18 @@ namespace PhotoReviewer
 
         private bool fullImageLoaded;
 
+        private bool isFullHeight;
+
         public PhotoView([NotNull] Photo selectedPhoto, [NotNull] IList<PhotoView> photoViews, [NotNull] MainWindow mainWindow)
         {
             InitializeComponent();
             this.mainWindow = mainWindow;
             PhotoZoomBorder.SetAction(LoadFullImage);
             this.photoViews = photoViews;
+            var isFirst = !photoViews.Any();
             photoViews.Add(this);
             Show();
-            ArrangeWindows();
+            ArrangeWindows(isFirst);
             SelectedPhoto = selectedPhoto;
             ViewedPhoto.Source = SelectedPhoto.Image;
 
@@ -134,6 +138,22 @@ namespace PhotoReviewer
             ChangePhoto(SelectedPhoto.Next);
         }
 
+        private void FullscreenButton_Click([NotNull] object sender, [NotNull] RoutedEventArgs e)
+        {
+            ToggleFullHeight();
+        }
+
+        private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            if (!e.HeightChanged)
+                return;
+            var fullHeight = Screen.FromHandle(new WindowInteropHelper(mainWindow).Handle).WorkingArea.Height;
+            var newHeight = e.NewSize.Height;
+            var newSizeIsFullHeight = Math.Abs(newHeight - fullHeight) < 50;
+            if (newSizeIsFullHeight != isFullHeight)
+                ToggleFullheightImage(this, newSizeIsFullHeight);
+        }
+
         private void Window_Closed([NotNull] object sender, [NotNull] EventArgs e)
         {
             photoViews.Remove(this);
@@ -184,7 +204,7 @@ namespace PhotoReviewer
             SelectAndAct(() => PhotoZoomBorder.Reset());
         }
 
-        private void ArrangeWindows()
+        private void ArrangeWindows(bool defaultHeight = false)
         {
             const double borderWidth = 8;
             var scr = Screen.FromHandle(new WindowInteropHelper(mainWindow).Handle);
@@ -194,15 +214,30 @@ namespace PhotoReviewer
                 double left = scr.WorkingArea.Left;
                 var thirdHeight = scr.WorkingArea.Height / 3;
                 var twoThirdsHeight = thirdHeight * 2;
+                double firstTop, firstHeight;
+                bool isFirstFullHeight;
+                if (defaultHeight)
+                {
+                    firstTop = scr.WorkingArea.Y + thirdHeight - borderWidth;
+                    firstHeight = twoThirdsHeight + 2 * borderWidth;
+                    isFirstFullHeight = false;
+                }
+                else
+                {
+                    var first = photoViews.First();
+                    firstTop = first.Top;
+                    firstHeight = first.Height;
+                    isFirstFullHeight = first.isFullHeight;
+                }
+                var actualWidth = width + 2 * borderWidth;
                 foreach (var photoView in photoViews)
                 {
-                    photoView.WindowStartupLocation = WindowStartupLocation.Manual;
-                    photoView.WindowState = WindowState.Normal;
                     photoView.Left = left - borderWidth;
                     left += width;
-                    photoView.Width = width + 2 * borderWidth;
-                    photoView.Top = scr.WorkingArea.Y + thirdHeight - borderWidth;
-                    photoView.Height = twoThirdsHeight + 2 * borderWidth;
+                    photoView.Width = actualWidth;
+                    photoView.Top = firstTop;
+                    photoView.Height = firstHeight;
+                    ToggleFullheightImage(photoView, isFirstFullHeight);
                 }
                 photoViews.Last().FocusWindow();
                 mainWindow.WindowState = WindowState.Normal;
@@ -214,6 +249,30 @@ namespace PhotoReviewer
             else
                 mainWindow.WindowState = WindowState.Maximized;
             mainWindow.ScrollToSelected();
+        }
+
+        private static readonly BitmapSource FullScreenImg = new BitmapImage(new Uri(@"/img/FullScreen.png", UriKind.Relative));
+        private static readonly BitmapSource FullScreenExitImg = new BitmapImage(new Uri(@"/img/FullScreenExit.png", UriKind.Relative));
+
+        private void ToggleFullHeight()
+        {
+            var scr = Screen.FromHandle(new WindowInteropHelper(mainWindow).Handle);
+
+            if (isFullHeight)
+                ArrangeWindows(true);
+            else
+                foreach (var photoView in photoViews)
+                {
+                    photoView.Top = 0;
+                    photoView.Height = scr.WorkingArea.Height;
+                    ToggleFullheightImage(photoView, true);
+                }
+        }
+
+        private static void ToggleFullheightImage(PhotoView photoView, bool isFullheight)
+        {
+            photoView.FullheightImage.Source = isFullheight?FullScreenExitImg:FullScreenImg;
+            photoView.isFullHeight = isFullheight;
         }
 
         private void Favorite()
